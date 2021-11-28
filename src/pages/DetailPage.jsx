c4f4f8dac543f911759549edc51fedbad5dcc4ad
ifanramadhany <ifanpersie14@gmail.com>
2021-11-28 11:52:34 +0700

    nov28/11:57

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -14,7 +14,7 @@ export default function DetailPage() {
     (state) => state.productState
   );
 
-  // forceUpdate hook
+  // forceUpdate hook 
   function useForceUpdate() {
     const [value, setValue] = useState(0); // integer state
     return () => setValue((value) => value + 1); // update the state to force render
